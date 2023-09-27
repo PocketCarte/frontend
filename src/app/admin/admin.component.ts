8763669fc571906cb3fb3d8adee1690e6af854f5
Saexy <thekoala250303@gmail.com>
2023-09-27 12:37:49 -0300

    feat: Adicionando favicon e título das páginas

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent {
-
-  constructor (private route: ActivatedRoute) { 
-    console.log(this.route.url);
+  constructor(private route: ActivatedRoute, private title: Title) {
+    this.title.setTitle('PocketCarte | Administrativo');
   }
-
 }
--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-system',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./system.component.scss']
 })
 export class SystemComponent {
+
+  constructor (private title: Title) {
+    this.title.setTitle('PocketCarte');
+  }
 
 }

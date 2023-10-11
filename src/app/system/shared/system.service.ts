15314422b26afe601d24ae5a5c22bbb05e109efd
Pedro-Dom <pedroaugusto.dom@outlook.com>
2023-10-10 23:51:41 -0300

    Alterando rotas públicas

---
+++ src/app/system/shared/system.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject, Observable, firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SystemService {
+
+  public currentCategorySubject: BehaviorSubject<string | undefined | null> = new BehaviorSubject<string | undefined | null>(undefined);
+
+  constructor(private httpClient: HttpClient) {
+    this.initialCategory();
+  }
+
+  public async initialCategory(): Promise<void> {
+    const url = `${environment.apiUrl}/categories`;
+
+    const categories: any = await firstValueFrom(this.httpClient.get(url));
+
+    if(!this.currentCategorySubject.value){
+      this.currentCategorySubject.next(categories[0].id);
+    }
+  }
+
+  public changeCurrentCategory(category_id: string): void {
+    this.currentCategorySubject.next(category_id);
+  }
+
+  public async getCategories(): Promise<any> {
+    const url = `${environment.apiUrl}/categories`;
+
+    const categories: any = await firstValueFrom(this.httpClient.get(url));
+
+    return categories;
+  }
+
+  public async getProducts(categoryId: string): Promise<any> {
+    const url = `${environment.apiUrl}/categories/${categoryId}/products`;
+
+    const products = await firstValueFrom(this.httpClient.get(url));
+
+    return products;
+  }
+
+  public get currentCategory() {
+    return this.currentCategorySubject.asObservable();
+  }
+}
--- src/app/system/system-menu/system-menu.component.ts
+++ src/app/system/system-menu/system-menu.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SystemService } from '../shared/system.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-system-menu',
   templateUrl: './system-menu.component.html',
   styleUrls: ['./system-menu.component.scss']
 })
-export class SystemMenuComponent {
+export class SystemMenuComponent implements OnInit, OnDestroy{
 
+  public categories: any[] = [];
+  public products: any[] = [];
+
+  private subscriptions: Subscription = new Subscription();
+
+  constructor (public systemService: SystemService) { }
+
+  public ngOnInit(): void {
+    this.systemService.getCategories().then(result => {
+      this.categories = result;
+    })
+
+    this.subscriptions.add(
+      this.systemService.currentCategory.subscribe((result) => {
+        if(result){
+          this.systemService.getProducts(result).then(result => {
+            this.products = result;
+          })
+        }
+      })
+    )
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
